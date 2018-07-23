dd163fad2ba14c886d98e4738a685036d0bf5226
charlyChips <41596371+charlyChips@users.noreply.github.com>
2018-07-23 17:42:31 -0500

    Modificado desde Web
    Hola mundo Agregado desde Web

--- JavaTestGitHub/src/javatestgithub/JavaTestGitHub.java
+++ JavaTestGitHub/src/javatestgithub/JavaTestGitHub.java
@@ -17,6 +17,7 @@ public class JavaTestGitHub {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo git desde mac");
+        System.out.println("Hola mundo git desde Web");
     }
     
 }
